27c9f5512bd50bf964a316cea792a49f1f87fc2a
Python_coder_K <turntabroz@gmail.com>
2025-01-10 06:38:33 +0900

    atcoder

---
+++ 0110_1.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+print(a + b)
+
+
+c = int(input())
+d = int(input())
+
+print(c + d)
+
+n = int(input())
+print("Yes" if n >= 18 else "No")
---
+++ 0110_2.py
@@ -0,0 +1,6 @@
+n, x = map(int, input().split())
+l = list(map(int, input().split()))
+
+
+
+print(sum(l) - x)
