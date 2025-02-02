8778122ac84d27a412ce1ec35ba9017fbc13026d
Python_coder_K <turntabroz@gmail.com>
2025-02-02 23:47:49 +0900

    atcoder

---
+++ 0201_5.py
@@ -0,0 +1,18 @@
+d = input()
+
+if d == "N":
+    print("S")
+elif d == "S":
+    print("N")
+elif d == "W":
+    print("E")
+elif d == "E":
+    print("W")
+elif d == "NE":
+    print("SW")
+elif d == "SW":
+    print("NE")
+elif d == "SE":
+    print("NW")
+elif d == "NW":
+    print("SE")
---
+++ 0201_6.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+l1 = [list(input()) for i in range(n)]
+l2 = [list(input()) for i in range(m)]
+
+
+print(l1)
+print(l2)
