ee19675bf5d3e07f09d77212e4e547f22f03b84a
Python_coder_K <turntabroz@gmail.com>
2025-04-04 07:29:26 +0900

    atcoder

---
+++ 0403_1.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+print(n + m)
+
+a = int(input())
+b = int(input())
+print(a + b)
+
+
+ans = 0
+for i in range(7):
+    tt = int(input())
+    ans += tt
+
+print(ans)
+
